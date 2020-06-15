e6f26d0e53606b1dfdc9200fea54f7fce53516ee
Kaustav <kaustav.shouvik00@gmail.com>
2020-06-15 20:26:26 +0530

    style colorboxes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends React.Component{
     render(){
+        const {color, name} = this.props;
         return (
-            <div style={{background: this.props.color}} className="ColorBox">
-                <span>{this.props.name}</span>
-                <span>MORE</span>
-            </div>
+            <CopyToClipboard text="hello">
+                <div style={{background : color}} className="ColorBox">
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="copy-button">COPY</button>
+                    </div>
+                    <span className="see-more">MORE</span>
+                </div>
+            </CopyToClipboard>
         );
     }
 }
