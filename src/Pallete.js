5bea8be2a992708660ef456f710354bb54cb79d7
Kaustav <kaustav.shouvik00@gmail.com>
2020-06-15 17:52:28 +0530

    start color pallete element

---
+++ src/Pallete.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Pallete extends React.Component{
+    render(){
+        return (
+            <div className="Pallete">
+                <h1>Pallete</h1>
+            </div>
+        );
+    }
+}
+
+export default Pallete;
