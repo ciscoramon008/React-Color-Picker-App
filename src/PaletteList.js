bfcca719f06ddc33360656bb60a9458f9fcb4d5f
Kaustav <kaustav.shouvik00@gmail.com>
2020-06-20 13:11:06 +0530

    add links to go to individual palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,7 +6,7 @@ import {withStyles} from '@material-ui/styles';
 const styles = {
     root: {
         background: 'blue',
-        height: '100%',
+        height: '100vh',
         display: 'flex',
         alignItems: 'flex-start',
         justifyContent: 'center',
@@ -32,11 +32,15 @@ const styles = {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%',
+        gridGap: '3%',
     }
 }
 
 class PaletteList extends React.Component{
+    goToPalette(id){
+        this.props.history.push(`/palette/${id}`);
+    }
+
     render(){
         const {palettes, classes} = this.props;
 
@@ -48,7 +52,7 @@ class PaletteList extends React.Component{
                         <button>Create Palette</button>
                     </nav>
                     <div className={classes.palettes}>
-                        {palettes.map(palette => <MiniPalette {...palette}/>)}
+                        {palettes.map(palette => <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} />)}
                     </div>
                 </div>
             </div>
